4f754686ddb8bbeea82795dc3d8b0836aa498ce8
melle <melvinhall54@gmail.com>
2023-03-23 11:37:30 +0100

    Java FX init

--- src/main/java/view/MainApplication.java
+++ src/main/java/view/MainApplication.java
@@ -1,22 +1,20 @@
 package view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Label label = new Label("Press space to play");
+        label.setTextFill(Color.rgb(255, 255, 255, 1));
 
         StackPane root = new StackPane();
         root.setStyle("-fx-background-color: black;");
