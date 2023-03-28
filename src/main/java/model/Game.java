0e0b61b3d848f7c6aaad1bf8e42f26d8aa33f616
Marcus <marcus.hakans@live.se>
2023-03-28 11:17:58 +0200

    highscore

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -6,6 +6,10 @@ import model.characters.Character;
 import model.characters.Projectile;
 import model.characters.Ship;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -19,10 +23,22 @@ public class Game {
     public int width = 0;
     public int height = 0;
     private boolean isRunning;
+    private FileWriter fileWriter;
+    private BufferedReader br;
+    private String fileName = "C:\\Users\\marcu\\Asteroids\\highscore.txt";
+
+
 
     public Game(int width, int height){
         this.width = width;
         this.height = height;
+        if(fileWriter == null) {
+            try {
+                fileWriter = new FileWriter(fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void start(){
@@ -41,9 +57,16 @@ public class Game {
             asteroids.add(asteroid);
         }
     }
-    public void stop(){
-        isRunning = false;
-    }
+    public void stop() {
+            // Check if current score is a new high score
+            boolean isHighScore = isHighScore();
+            if (isHighScore) {
+                writeHighScore();
+            }
+            isRunning = false;
+        }
+
+
 
     public Character addProjectile(){
         Projectile projectile = new Projectile((int) ship.getX(), (int) ship.getY());
@@ -84,5 +107,30 @@ public class Game {
 
     public boolean isRunning() {
         return isRunning;
+    }
+
+    public boolean isHighScore() {
+        int highScore = 0;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line = br.readLine();
+            if (line != null) {
+                highScore = Integer.parseInt(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return points > highScore;
+    }
+
+    public void writeHighScore() {
+        try {
+            fileWriter = new FileWriter(fileName);
+            fileWriter.write(String.valueOf(points));
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
