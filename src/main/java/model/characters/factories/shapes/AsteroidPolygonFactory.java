45a9e7367e8bfee0f7ffc63b9ce1035893012db2
melle <melvinhall54@gmail.com>
2023-03-30 16:50:03 +0200

    nu slumpas även antalet hörn på asteroiderna och inte bara storleken

--- src/main/java/model/characters/factories/shapes/AsteroidPolygonFactory.java
+++ src/main/java/model/characters/factories/shapes/AsteroidPolygonFactory.java
@@ -9,25 +9,19 @@ public class AsteroidPolygonFactory {
     private AsteroidPolygonFactory() {}
     public Polygon createPolygon() {
         Random rnd = new Random();
-
+        int nrOfVertices = rnd.nextInt(6) + 5;
         double size = 10 + rnd.nextInt(10);
-
         Polygon polygon = new Polygon();
-        double c1 = Math.cos(Math.PI * 2 / 5);
-        double c2 = Math.cos(Math.PI / 5);
-        double s1 = Math.sin(Math.PI * 2 / 5);
-        double s2 = Math.sin(Math.PI * 4 / 5);
 
-        polygon.getPoints().addAll(
-                size, 0.0,
-                size * c1, -1 * size * s1,
-                -1 * size * c2, -1 * size * s2,
-                -1 * size * c2, size * s2,
-                size * c1, size * s1);
+        for (int i = 0; i < nrOfVertices; i++) {
+            double angle = 2 * Math.PI * i / nrOfVertices;
+            double x = size * Math.cos(angle);
+            double y = size * Math.sin(angle);
+            polygon.getPoints().addAll(x, y);
+        }
 
         for (int i = 0; i < polygon.getPoints().size(); i++) {
-            int change = rnd.nextInt(5) - 2;
-            polygon.getPoints().set(i, polygon.getPoints().get(i) + change);
+            polygon.getPoints().set(i, polygon.getPoints().get(i) + rnd.nextInt(10) - 5);
         }
 
         return polygon;
